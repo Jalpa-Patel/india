235ccd46a156f0e8aa2506098fc8b126361c48b6
jasvinder <jasvinder11@uni-koblenz.de>
2016-11-22 23:54:21 +0100

    Final submission for Assignment 4.   * Question 1 solution: http_client.py which sees the http status code 200 and then saves the header and      body of the url in two different files. If not 200 nothing is saved.   * Question 2 solution: downloadEverything.py which prints the all Images Urls and saves the image file.

--- assignment4/India_assignment4_Q2client.py
+++ assignment4/India_assignment4_Q2client.py
@@ -4,8 +4,8 @@ import sys
 __author__ = "jasvinder"
 
 
-# creates an INET, STREAMING socket
-# input:
+# create an INET, STREAMING socket
+# input: 
 #   url:String - takes the valid url to make tcp connection
 #   port: Integer - take the port number to connect
 # output:
@@ -15,41 +15,51 @@ def tcp_connection(url, port):
   clientSocket.connect((url, port))
   return clientSocket
 
+
+
+
 # Parses the URL into six components.i.e scheme, network location, path, parameters, query and fragment
-# input:
+# input: 
 #   url_name:String - takes the valid url passed
 # output:
 #   String - parsed Url into 6 components
+
 def parse_url(url_name):
   return urlparse(url_name)
 
 # User request for sending user input URL as HTTP request ending with two empty lines
 # input:
-#   url_name:String - takes the valid url passed
+#   url_name:String - takes the valid url passed 
 #   method_type:String - takes Http method type,i.e generally, POST or GET
 # output:
 #   get_request:String - returns the valid Http Request according to method type
+
 def encode_http_request(path, method_type):
   protocol_type = 'HTTP/1.0'
   get_request = ''.join([method_type,' ', path,' ',protocol_type, '\r','\n','\r','\n'])
   return get_request
 
-# Sends the passed http_request request over passed tcp socket object.
+# Sends the Http Request 
 # input:
 #   tcp_connection:TCP Socket object - takes the object of the TCP connection established
 #   http_request:String - takes the valid Http Request according to method type i.e GET or POST method
-# output:
+# output: 
 #   Bytes: returns the Http request made in bytes to send it further for communication
 def send_http_request(tcp_connection, http_request):
   return tcp_connection.send(str.encode(http_request))
 
-# Reads the Http header of the response from tcp_connection socket.
-# input:
-#   tcp_connection:TCP Socket object - takes the object of the TCP connection established
-#output:
-#   http_headers: String - gives the HTTP header of the response
+# Receives the Http Response
+def receive_http_body_as_text(tcp_connection):
+  total_http_response_data = []
+  http_response = tcp_connection.recv(512).decode('utf-8')
+  total_http_response_data.append(http_response)
+  while(http_response):
+    http_response = tcp_connection.recv(512).decode('utf-8')
+    total_http_response_data.append(http_response)
+  return ''.join(total_http_response_data)
+
 def read_http_header(tcp_connection):
-  http_headers = ['HTTP/1.1 200 OK']# initialising headers too 22 as it has already been read to determine whether its 200 or not.
+  http_headers = []
   while(True):
     response = tcp_connection.recv(1).decode('utf-8')
     if(response == '\r'):
@@ -60,49 +70,24 @@ def read_http_header(tcp_connection):
         break
       else:
         http_headers.append(response)
-
+    
     else:
       http_headers.append(response)
   return ''.join(http_headers)
-
-# Checks whether the response is 200 or not from the tcp socket.
-# input:
-#   tcp_connection:TCP Socket object - takes the object of the TCP connection established
-#output:
-#    Boolean : True for 200 False for all other http status code.
-def http_status_200(tcp_connection):
-  http_status = tcp_connection.recv(15).decode('utf-8')
-  print(http_status)
-  if(http_status.startswith('HTTP/1.1 200 OK')):
-    return True
-  return False
-
-# Reads the body of the Http response as binary
-# input:
-#   tcp_connection:TCP Socket object - takes the object of the TCP connection established
-#output:
-#   list - gives the body of the Http response, as binary data
+  
 def read_http_body_as_binary(tcp_connection):
   http_body = []
   response = tcp_connection.recv(1024)
   while(response):
     http_body.append(response)
     response = tcp_connection.recv(1024)
+    #http_body.append(response)
   return http_body
 
-# Decodes http header
-# input:
-#   http_header: Http header string.
-#output:
-#    list - list of http header strings.
 def decode_http_header(http_header):
   return http_header.strip().split('\r\n')
+  # { value.split(':')[0] : value.split(':')[1] for value in http_header.split('\r\n')}
 
-# Reads the decoded http header
-# input:
-#   decoded_http_header: list of decoded http header
-#output:
-#    'String: tells whether type is text in binary
 def find_content_type(decoded_http_header):
   #print(decoded_http_header)
   for value in decoded_http_header:
@@ -112,22 +97,11 @@ def find_content_type(decoded_http_header):
       else:
         return 'binary'
 
-# Reads the string and the file name and saves the string on the file.
-# input:
-#   string_data_to_be_written:String - String of text to be written on the file
-#   file_name: String- file name.
-#output:
-#    None
+
 def save_text_data(string_data_to_be_written, file_name):
   with open(file_name, 'w+') as file:
     file.write(string_data_to_be_written)
 
-# Reads the string and the file name and saves the string on the file.
-# input:
-#   binary_data_array_to_be_written:List - List of Byte to be written on the file
-#   file_name: String- file name.
-#output:
-#    None
 def save_binary_data(binary_data_array_to_be_written, file_name):
     #binary_data_to_be_written = str.encode(string_data_to_be_written)
     with open(file_name, 'bw') as file:
@@ -136,34 +110,23 @@ def save_binary_data(binary_data_array_to_be_written, file_name):
 
 
 
-# Reads the string and the file name and saves the string on the file.
-# input:
-#   url_name:String - Url of the resource to be downloaded.
-#   saveheader: Boolean- default is True. This is a flag for stating whether the header or not.
-#output:
-#    None
-def call_http_server(url_name, saveheader = True):
+#Sending data from client to server
+def http_server(url_name, saveheader = True):
   parsed_url = parse_url(url_name)
   tcp_socket = tcp_connection(parsed_url.netloc, 80)
   http_request = encode_http_request(parsed_url.path, 'GET')
   send_http_request(tcp_socket, http_request)
-  print(url_name)
-  if(http_status_200(tcp_socket)):
-    http_header = read_http_header(tcp_socket)
-    http_data = read_http_body_as_binary(tcp_socket)
-    decoded_message = decode_http_header(http_header)
-    file_name = parsed_url.path.split('/')[-1]
-    if saveheader:
-      header_file_name = file_name +  '.header'
-      save_text_data(http_header, header_file_name)
-    if(find_content_type(decoded_message) == 'text'):
-      text_file_name = file_name + '.html'
-      save_binary_data(http_data, text_file_name)
-    elif(find_content_type(decoded_message) == 'binary'):
-      save_binary_data(http_data, file_name)
-  else:
-      print("http status not 200")
+  http_header = read_http_header(tcp_socket)
+  http_data = read_http_body_as_binary(tcp_socket)
+  decoded_message = decode_http_header(http_header)
+  file_name = parsed_url.path.split('/')[-1]
+  if saveheader:
+    header_file_name = file_name +  '.header' 
+    save_text_data(http_header, header_file_name)
+  if(find_content_type(decoded_message) == 'text'):
+    text_file_name = file_name + '.html'
+    save_binary_data(http_data, text_file_name)
+  elif(find_content_type(decoded_message) == 'binary'):
+    save_binary_data(http_data, file_name)
   tcp_socket.close()
-
-if __name__ == "__main__":
-  call_http_server(sys.argv[1])
+http_server(sys.argv[1])
--- assignment4/downloadEverything.py
+++ assignment4/downloadEverything.py
@@ -1,46 +1,60 @@
 import re
 import sys
 import http_client
+
+# Reads the file saved at the file name and returns the data.
+# input:
+#   file_name:String - name of the fiel to be read.
+#output:
+#    String: Data from the file.
 def read_data(file_name):
   with open(file_name, 'r') as f:
      data = f.read()
   return data
 
 
+# finds the src from the img tag in full html string..
+# input:
+#   html:String - HTML data as strings
+#output:
+#    List: list of urls from image tags src.
 def match_img_src(html):
   img_pattern = '<img.*?src="([^"]*)"'
   return re.findall(img_pattern, html)
 
 
+# Converts the relative urls from url list to complete http urls.
+# input:
+#   url_list:List - URL list as list of string.
+#   url: String- Url of main page from where html was downloaded.
+#output:
+#    List - List of complete http url.
 def make_absolute_url(url_list, url):
   downloaded_domain = http_client.parse_url(url).netloc
   complete_url = []
   for url in url_list:
     parsed_url = http_client.parse_url(url)
-#    parsed_url.append(http_client.parse_url(url_list))
     if(parsed_url.netloc):
       complete_url.append(url)
     else:
       complete_url.append('http://' + downloaded_domain + url )
   return complete_url
 
-
-
-def print_all_url_links(file_name):
+# Reads the html files extracts all html links to image and download it and saves it to the disc and prints all the URLs at the console.
+# input:
+#   file_name - Name of the file in which html page is saved-
+#   url:String - Url of the resource from where main file was  downloaded.
+#output:
+#    None
+def download_urls(file_name, url):
   html_data = read_data(file_name)
   url_list = match_img_src(html_data)
-  return url_list
-
-
-def download_urls(file_name,url):
-  url_list = print_all_url_links(file_name)
   full_url_list = make_absolute_url(url_list, url)
+  print('List of URLs extracted from downloaded html')
   print(full_url_list)
   for url in full_url_list:
-    http_client.http_server(url, False)
-
-
-
+    print('\ndownloading url')
+    http_client.call_http_server(url, False)
 
 if __name__ == "__main__":
   download_urls(sys.argv[1], sys.argv[2])
